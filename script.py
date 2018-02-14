0c040de5cb965679d6d081d5e591d5098e29e27a
Zhankin <zhankin@mail.ru>
2018-02-14 17:57:34 +0600

    Update script.py

--- script.py
+++ script.py
@@ -25,5 +25,5 @@ def webhook():
     bot.set_webhook(url="https://zhankin-test-1.herokuapp.com/510800243:AAEsmyadUWf8h6VL4YV0HbjXtvuYVLRNpFQ")
     return "!", 200
 
-server.run(host="0.0.0.0", port=5000)
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 server = Flask(__name__)
